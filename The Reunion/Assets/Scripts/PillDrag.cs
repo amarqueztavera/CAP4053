72ca550a7536d5af8d53dce707ebe8e421d2c8da
Sam41704 <sam41704@gmail.com>
2025-03-09 03:05:54 -0400

    Pill Puzzle Done
    could be improved, its very basic rn

---
+++ The Reunion/Assets/Scripts/PillDrag.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PillDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Transform originalParent;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        if (canvasGroup == null)
+        {
+            canvasGroup = gameObject.AddComponent<CanvasGroup>(); // Adds it if missing
+        }
+
+        originalParent = transform.parent;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+        if (transform.parent == originalParent)
+        {
+            rectTransform.anchoredPosition = Vector3.zero;
+        }
+    }
+}
---
+++ The Reunion/Assets/Scripts/PillManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PillManager : MonoBehaviour
+{
+    public static PillManager Instance;
+    public GameObject winText;
+
+    private Dictionary<string, int> placedPills = new Dictionary<string, int>(); // Tracks placed pills by color
+    private int totalPillsPerColor = 3; // Number of pills per color
+    private int totalColors = 3; // Number of different pill colors
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void RegisterPill(GameObject pill, string pillColor)
+    {
+        if (!placedPills.ContainsKey(pillColor))
+        {
+            placedPills[pillColor] = 0; // Initialize count for this color
+        }
+
+        placedPills[pillColor]++; // Increment count for this color
+
+        if (AllPillsSorted())
+        {
+            ShowWinMessage();
+        }
+    }
+
+    private bool AllPillsSorted()
+    {
+        // If there are not enough colors sorted, return false
+        if (placedPills.Count < totalColors)
+        {
+            return false;
+        }
+
+        // Check that every color has the required number of pills sorted
+        foreach (var pillCount in placedPills.Values)
+        {
+            if (pillCount < totalPillsPerColor)
+            {
+                return false;
+            }
+        }
+        return true; // All colors have been sorted correctly
+    }
+
+    public void ShowWinMessage()
+    {
+        if (winText != null)
+        {
+            winText.SetActive(true);
+        }
+        Debug.Log("You Win!");
+    }
+}
