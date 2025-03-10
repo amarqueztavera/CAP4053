c034eea68ed37b53100be96d649a98877c31e31a
Andrea Marquez Tavera <amarqueztavera@yahoo.com>
2025-03-10 07:17:27 -0400

    clue

--- The Reunion/Assets/Scripts/ClueCounter.cs
+++ The Reunion/Assets/Scripts/ClueCounter.cs
@@ -42,7 +42,7 @@ public class ClueCounter : MonoBehaviour
         DoorLockController.UpdateAllLocks(currentAct); // Update door locks
 
         // Check for ending
-        if (clueCount > 3)// edit to 9 after prototype
+        if (clueCount >= 3)// edit to 9 after prototype
         {
             SceneManager.UnloadSceneAsync("Map");
             SceneManager.UnloadSceneAsync("GameUI");
