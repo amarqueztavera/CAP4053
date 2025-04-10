c8c9ae04c32572d977ca28fafa07879644bb824b
Jandro-WDS <125507795+Jandro-WDS@users.noreply.github.com>
2025-04-10 16:56:28 -0400

    npc random, act based  movement
    NPC move randomly, and can only access act specific parts

--- The Reunion/Assets/Scripts/DELETE.cs
+++ The Reunion/Assets/Scripts/DELETE.cs
@@ -6,6 +6,11 @@ using UnityEngine.AI;
 
 public class DELETE : MonoBehaviour
 {
+    public bool act1 = true;
+    public bool act2 = false;
+    public bool act3 = false;
+
+
     [SerializeField] List<Transform> targets = new List<Transform>();
     public int wayPointIndex=0;
 
@@ -19,7 +24,8 @@ public class DELETE : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation =  false;
         agent.updateUpAxis = false;
-        
+
+        wayPointIndex = selectWaypointIndex();
     }
 
     void Update()
@@ -39,12 +45,11 @@ public class DELETE : MonoBehaviour
         Vector3 currentPosition = transform.position;
         var distance = Vector3.Distance(currentPosition, targets[wayPointIndex].position);
 
-        Debug.Log("NPC POS: " + currentPosition + "Target pos:" + targets[wayPointIndex].position + "Distance:" + distance);
+        //Debug.Log("NPC POS: " + currentPosition + "Target pos:" + targets[wayPointIndex].position + "Distance:" + distance);
 
         if (distance <= 1.0f)
         {
-            wayPointIndex++;
-
+            wayPointIndex = selectWaypointIndex();
             yield return new WaitForSeconds(3);
         }
 
@@ -53,6 +58,32 @@ public class DELETE : MonoBehaviour
         yield return null;
 
 
+    }
+
+    int selectWaypointIndex()
+    {
+        wayPointIndex = Random.Range(0, targets.Count);
+        while (!isValidWaypoint(wayPointIndex))
+        {
+            wayPointIndex = Random.Range(0, targets.Count);
+        }
+        return wayPointIndex;
+    }
+
+    bool isValidWaypoint( int wayPointIndex)
+    {
+        string tag = targets[wayPointIndex].tag;
+
+        Debug.Log("checking tag: "+ tag);
+
+        if (tag == "act1 waypoint")
+            return act1;
+        if (tag == "act2 waypoint" )
+            return act2;
+        if (tag == "act3 waypoint") 
+            return act3;
+
+        return false;
     }
 
 }
