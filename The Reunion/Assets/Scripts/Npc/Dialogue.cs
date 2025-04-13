d6166398e7866eb9d61535896ff9182643d8635d
Jandro-WDS <125507795+Jandro-WDS@users.noreply.github.com>
2025-04-13 19:15:30 -0400

    NPC dialogue

--- The Reunion/Assets/Scripts/Npc/Dialogue.cs
+++ The Reunion/Assets/Scripts/Npc/Dialogue.cs
@@ -1,8 +1,19 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public GameObject continueButton;
+    public float wordSpeed;
+    public bool playerIsClose;
     void Start()
     {
         
@@ -11,6 +22,71 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.E) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+
+        if(dialogueText.text == dialogue[index])
+        {
+            continueButton.SetActive(true); 
+        }
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+
+        continueButton.SetActive(false) ;  
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else 
+        {
+            zeroText();
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            playerIsClose = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+
     }
 }
